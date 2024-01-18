96b4cec7d751187bd7c4b569350f06886fcdd668
Elena Padun <whiteangel64rus@gmail.com>
2024-01-18 17:57:43 +0300

    feat: addded ui component Button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,13 @@
 import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
-
+import SiteBar from '@/components/shared/sitebar';
 const cx = classNames.bind(styles);
 
 const HomePage = () => {
   return (
     <div className={cx('wrapper')}>
       <h2>HomePage</h2>
+      <SiteBar />
     </div>
   );
 };
