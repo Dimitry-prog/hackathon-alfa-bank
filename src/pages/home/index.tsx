dca309dca961471e0c21bc8575908433f530737b
Dmitry <kfifa3@gmail.com>
2024-01-19 18:08:10 +0300

    fix: delete import

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,12 @@
 import classNames from 'classnames/bind';
 import styles from './styles.module.scss';
-import DpdForm from '@/components/shared/dpd-form';
 
 const cx = classNames.bind(styles);
 
 const HomePage = () => {
   return (
     <div className={cx('wrapper')}>
-      <DpdForm />
+      <h2>HomePage</h2>
     </div>
   );
 };
