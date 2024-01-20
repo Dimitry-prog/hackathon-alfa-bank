1e246e945a7896543782693799051033b07ff79f
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2024-01-20 13:20:07 +0300

    fix/header

---
+++ src/libs/constants.ts
@@ -0,0 +1,17 @@
+export const headerLinks = [
+  {
+    id: '1',
+    label: 'Контакты',
+    href: '/contacts',
+  },
+  {
+    id: '2',
+    label: 'Всё о работе',
+    href: '/all_about_work',
+  },
+  {
+    id: '3',
+    label: 'Подразделения',
+    href: '/divisions',
+  },
+];
