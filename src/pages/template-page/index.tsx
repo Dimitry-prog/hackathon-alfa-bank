bc27495407370a8e7fe950ef9a1d06f13e1d5564
Ekaterina Danilova <ekat.sokolova2010@yandex.ru>
2024-01-28 23:44:19 +0300

    fix/docker

--- src/pages/template-page/index.tsx
+++ src/pages/template-page/index.tsx
@@ -5,6 +5,9 @@ import TemplateForm from '@/components/shared/template-form';
 const cx = classNames.bind(styles);
 
 const TemplatePage = () => {
+  // ниже 2 строки для проверки запроса
+  // const { data, error, isLoading } = useGetTaskQuery(1);
+  // console.log(data, error, isLoading);
   return (
     <section className={cx('wrapper')}>
       <h2>Конструктор ИПР</h2>
